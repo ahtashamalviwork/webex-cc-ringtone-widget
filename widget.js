4ceb63d725e620fee0f1b3b3c4df4abdec548c6e
ahtashamalviwork <ahtasham.alvi.work@gmail.com>
2025-07-02 10:10:15 -0500

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,11 +1,6 @@
 (function () {
   const test = document.createElement('div');
-  test.style.fontSize = '13px';
-  test.style.color = 'white';
-  test.style.backgroundColor = '#0a74da';
-  test.style.padding = '5px 10px';
-  test.style.margin = '4px';
-  test.style.borderRadius = '4px';
+  ...
   test.textContent = '✅ Ringtone Widget Loaded';
   document.body.appendChild(test);
 })();
